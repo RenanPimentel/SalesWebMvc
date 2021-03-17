2e7864d0baec0dd2f0b6e6dcad41c4fd5a6d2744
RenanPimentel <renansinhorini12@gmail.com>
2021-03-17 17:05:33 -0300

    Added SellerService and basic FindAll method

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using SalesWebMvc.Services;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,18 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var services = _sellerService.FIndAll();
+
+            return View(services);
         }
     }
 }
