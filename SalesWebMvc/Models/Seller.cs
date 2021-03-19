1abed546b5934f5952653ac38772ab03cd67feed
RenanPimentel <renansinhorini12@gmail.com>
2021-03-19 15:51:23 -0300

    Formatted number and date in Seller class and modified culture info

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -8,12 +9,23 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
-        public double BaseSalary { get; set; }
         public Department Department { get; set; }
-        public int DepartmentId { get; set; } // foreign key
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:f2}")]
+        public double BaseSalary { get; set; }
+
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; } // foreign key
 
         public Seller() { }
 
